10fb4c5a90c4226f69019800cf08c466ff6e3581
RuslanAkhmetov <akhmrusl@gmail.com>
2023-03-15 21:02:11 +0300

    Add base layout

--- app/src/main/java/ru/geekbrain/android/gitapp/MainActivity.kt
+++ app/src/main/java/ru/geekbrain/android/gitapp/MainActivity.kt
@@ -2,10 +2,19 @@ package ru.geekbrain.android.gitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import ru.geekbrain.android.gitapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.refreshButton.setOnClickListener{
+            Toast.makeText(this, "hello!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
